a0a0f54f2df7c7daf2382a290057cc1db84ea44f
enxajt <enxajt@gmail.com>
2017-03-15 19:09:46 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('ejs', function() {
     .pipe(replace(/^# (.*)$/gm, '  <h1>$1</h1>'))
     .pipe(replace(/^## (.*)$/gm, '  <h2>$1</h2>'))
     .pipe(replace(/^/, "<div class='step' >\n"))
-    .pipe(replace(/^---$/gm, "<div class='step' >"))
+    .pipe(replace(/^---$/gm, "</div>\n\n<div class='step' >"))
 
     .pipe(rename('_pages.ejs'))
     .pipe(gulp.dest(_path.src))
