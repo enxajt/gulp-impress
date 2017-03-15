7d41839dfb512fdd0590ee4b081e8b9a3c0141ec
enxajt <enxajt@gmail.com>
2017-03-15 16:39:59 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -31,11 +31,13 @@ gulp.task('ejs', function() {
   return gulp.src(_path.src+'/*.impress.md')
     .pipe(cached('ejs'))
     .pipe(tap(function(file,t) {
-      console.log('file.path: '+file.path);
       var filename = path.basename(file.path);
-      console.log('filename: '+filename);
+      var title = filename.split(/\.(?=[^.]+$)/)[0];
+      console.log('title0: '+title);
       var title = filename.split(/\.(?=[^.]+$)/)[1];
-      console.log('title: '+title);
+      console.log('title1: '+title);
+      var title = filename.split(/\.(?=[^.]+$)/)[2];
+      console.log('title2: '+title);
       var css = title+'.css';
       gulp.src(["./ejs/index.html","!./ejs/*.ejs"])
         .pipe(ejs({
