454433ff7436c7c6343f5e9e4cff49e22b812159
enxajt <enxajt@gmail.com>
2017-03-15 18:46:25 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -32,8 +32,8 @@ gulp.task('ejs', function() {
   gulp.src(_path.src+'/*.impress.md')
     //.pipe(cached('ejs'))
 
-    .pipe(replace(/^# \(.*\)$/g, '<h1>\1</h1>'))
-    .pipe(replace(/#/g, '<h></h>'))
+    .pipe(replace(/# \(.*\)/g, '<h1>$1</h1>'))
+    .pipe(replace(/^#/g, '<h></h>'))
 
     .pipe(rename('_pages.ejs'))
     .pipe(gulp.dest(_path.ejs))
