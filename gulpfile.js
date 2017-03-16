7077073925c94d0eac32db39a2fd22d6d1856548
enxajt <enxajt@gmail.com>
2017-03-16 15:08:09 +0100

    rm test

--- gulpfile.js
+++ gulpfile.js
@@ -39,11 +39,11 @@ gulp.task('ejs', function() {
       gulp.src('./')
         .pipe(exec('rm -f ./src/_pages.md'))
         .pipe(exec('rm -f ./src/_pages.ejs'))
-        .pipe(exec('rm -f ./src/'+title+'.html'))
-        .pipe(exec('cp ./src/'+filename+' ./src/_pages.md'))
-        .pipe(exec('sh ./src/replace.sh'))
-        .pipe(exec('cp ./src/_pages.ejs ./ejs/_pages.ejs'))
-        .pipe(exec('cp ./src/impress.css ./src/"'+title+'".css'));
+        .pipe(exec('rm -f ./src/'+title+'.html'));
+        //.pipe(exec('cp ./src/'+filename+' ./src/_pages.md'))
+        //.pipe(exec('sh ./src/replace.sh'))
+        //.pipe(exec('cp ./src/_pages.ejs ./ejs/_pages.ejs'))
+        //.pipe(exec('cp ./src/impress.css ./src/"'+title+'".css'));
       gulp.src(["./ejs/index.html","!./ejs/*.ejs"])
         .pipe(ejs({
           title: title,
