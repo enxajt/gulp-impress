b21863519df2281fc921c0191f8ec91ecb6befec
enxajt <enxajt@gmail.com>
2017-03-22 16:12:38 +0100

    try pandoc for error in ejs

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,7 @@ var webserver = require('gulp-webserver');
 var print = require('gulp-print');
 var tap = require('gulp-tap');
 var exec = require('gulp-exec');
-var ejs = require('gulp-ejs');
-var rename = require('gulp-rename');
 var cached = require('gulp-cached');
-var plumber = require('gulp-plumber');
-var fs = require('fs');
-var replace = require('gulp-replace');
-var fs = require("fs");
-
-var _path = {
-  src : './src',
-  ejs : './ejs'
-};
 
 gulp.task('webserver',function() {
   return gulp.src('./')
@@ -28,57 +17,40 @@ gulp.task('webserver',function() {
     }));
 });
 
-gulp.task('ejs', function() {
-  return gulp.src(_path.src+'/*.impress.md')
-    //.pipe(cached('ejs'))
+gulp.task('pandoc', function() {
+  return gulp.src('./src/*.impress.md')
+    //.pipe(cached('pandoc'))
     .pipe(tap(function(file,t) {
       var filename = path.basename(file.path);
       var title = filename.split(/\.(?=[^.]+$)/)[0];
       title = title.split(/\.(?=[^.]+$)/)[0];
       console.log('title: '+title);
+      var html = title+'.html';
       var css = title+'.css';
-      var sed = fs.readFileSync("./src/impress/replace.sh", "utf8");
-      gulp.src(["./ejs/index.html","!./ejs/*.ejs"])
-        .pipe(console.log('test: '+title))
-        .pipe(console.log('test2: '+title))
-        //.pipe(exec('cat '+file.path+' > '+sed+' > ./src/impress/'+title+'_pages.ejs'))
-        //.pipe(exec("ckt '"+file.path+"' > ./src/impress/"+title+"_pages.ejs"))
-        .pipe(exec('echo test'))
-        .pipe(exec('echo test > ./ttss'))
-        .pipe(exec('[ -e ./src/'+css+' ] || cp ./src/impress/template.css ./src/'+css))
-        .pipe(exec('rm -f ./src/impress/'+title+'.html'))
-        .pipe(fs.readFileSync('./src/impress/'+title+'_pages.ejs', "utf-8", function(err, _data) {
-          console.log('test_sed: ');
-          var pages = _data;
-          console.log('pages: '+pages);
-        }))
-        .pipe(ejs({
-          title: title,
-          css: css,
-          pages: pages
-        }))
-        .pipe(rename(title+'.html'))
-        .pipe(gulp.dest(_path.src))
+      var pdf = title+'.pdf';
+      gulp.src('./')
+        .pipe(exec('pandoc --template ./src/impress/template.html -V title='+title+' -s -t html5 --section-divs -o ./src/'+html+' ./src/'+title+'.impress.md'))
         .pipe(print(function(filepath) {
-          return "ejs: " + filepath;
+          return "made " + html;
         }))
-        //.pipe(cached('ejs'))
+        .pipe(exec('[ -e ./src/'+css+' ] && echo existing || cp ./src/impress/template.css ./src/'+css))
         .pipe(print(function(filepath) {
-          return "pdf-start";
-        }))
-        .pipe(exec('./decktape-1.0.0/phantomjs ./decktape-1.0.0/decktape.js impress ./src/'+title+'.html ./src/'+title+'.pdf'))
-        .pipe(print(function(filepath) {
-          return "pdf-end";
+          return "made " + css;
         }))
         .pipe(print(function(filepath) {
-          return "ejs-end";
+          return 'making '+pdf;
+        }))
+        .pipe(exec('[ -e ./src/'+pdf+' ] && rm ./src/'+pdf+' || echo nonexisting'))
+        .pipe(exec('./decktape-1.0.0/phantomjs ./decktape-1.0.0/decktape.js impress ./src/'+html+' ./src/'+pdf))
+        .pipe(print(function(filepath) {
+          return "made "+pdf;
         }));
     }));
 });
 
 gulp.task('watch', function() {
-  gulp.watch([_path.src+'/*.impress.md'],['ejs']);
+  gulp.watch(['./src/*.impress.md'],['pandoc']);
   gulp.src('gulpfile.js');
 });
 
-gulp.task('default', ['watch', 'webserver','ejs']);
+gulp.task('default', ['watch', 'webserver','pandoc']);
