c17368d65b14cac2b12232fbbb21886cce771428
enxajt <enxajt@gmail.com>
2017-03-22 13:26:04 +0100

    backupped pandoc, start edit ejs

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,17 @@ var webserver = require('gulp-webserver');
 var print = require('gulp-print');
 var tap = require('gulp-tap');
 var exec = require('gulp-exec');
+var ejs = require('gulp-ejs');
+var rename = require('gulp-rename');
 var cached = require('gulp-cached');
+var plumber = require('gulp-plumber');
+var fs = require('fs');
+var replace = require('gulp-replace');
+
+var _path = {
+  src : './src',
+  ejs : './ejs'
+};
 
 gulp.task('webserver',function() {
   return gulp.src('./')
@@ -17,40 +27,52 @@ gulp.task('webserver',function() {
     }));
 });
 
-gulp.task('pandoc', function() {
-  return gulp.src('./src/*.impress.md')
-    //.pipe(cached('pandoc'))
+// TODO
+pandoc --template ./src//impress-template.html -V title=test -s -t html5 --section-divs -o /vagrant/shared/test.html ~/.pandoc/test.impress.md  
+
+gulp.task('ejs', function() {
+  return gulp.src(_path.src+'/*.impress.md')
+    //.pipe(cached('ejs'))
     .pipe(tap(function(file,t) {
       var filename = path.basename(file.path);
       var title = filename.split(/\.(?=[^.]+$)/)[0];
       title = title.split(/\.(?=[^.]+$)/)[0];
       console.log('title: '+title);
-      var html = title+'.html';
       var css = title+'.css';
-      var pdf = title+'.pdf';
-      gulp.src('./')
-        .pipe(exec('pandoc --template ./src/impress/template.html -V title='+title+' -s -t html5 --section-divs -o ./src/'+html+' ./src/'+title+'.impress.md'))
-        .pipe(print(function(filepath) {
-          return "made " + html;
+      gulp.src(["./ejs/index.html","!./ejs/*.ejs"])
+        .pipe(exec('rm -f ./src/_pages.md'))
+        .pipe(exec('rm -f ./src/_pages.ejs'))
+        .pipe(exec('rm -f ./src/'+title+'.html'))
+        .pipe(exec('cp ./src/'+filename+' ./src/_pages.md'))
+        .pipe(exec('sh ./src/replace.sh'))
+        .pipe(exec('cp ./src/_pages.ejs ./ejs/_pages.ejs'))
+        .pipe(exec('if [ ! -e ./src/'+css+' ]; then cp ./src/impress.css ./src/'+css+' ; fi'))
+        .pipe(ejs({
+          title: title,
+          css: css
         }))
-        .pipe(exec('[ -e ./src/'+css+' ] && echo existing || cp ./src/impress/template.css ./src/'+css))
+        .pipe(rename(title+'.html'))
+        .pipe(gulp.dest(_path.src))
         .pipe(print(function(filepath) {
-          return "made " + css;
+          return "ejs: " + filepath;
+        }))
+        //.pipe(cached('ejs'))
+        .pipe(print(function(filepath) {
+          return "pdf-start";
+        }))
+        .pipe(exec('./decktape-1.0.0/phantomjs ./decktape-1.0.0/decktape.js impress ./src/'+title+'.html ./src/'+title+'.pdf'))
+        .pipe(print(function(filepath) {
+          return "pdf-end";
         }))
         .pipe(print(function(filepath) {
-          return 'making '+pdf;
-        }))
-        .pipe(exec('[ -e ./src/'+pdf+' ] && rm ./src/'+pdf+' || echo nonexisting'))
-        .pipe(exec('./decktape-1.0.0/phantomjs ./decktape-1.0.0/decktape.js impress ./src/'+html+' ./src/'+pdf))
-        .pipe(print(function(filepath) {
-          return "made "+pdf;
+          return "ejs-end";
         }));
     }));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['./src/*.impress.md'],['pandoc']);
+  gulp.watch([_path.src+'/*.impress.md'],['ejs']);
   gulp.src('gulpfile.js');
 });
 
-gulp.task('default', ['watch', 'webserver','pandoc']);
+gulp.task('default', ['watch', 'webserver','ejs']);
