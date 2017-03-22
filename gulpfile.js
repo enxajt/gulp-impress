61a1f1d14d7e9a91a66009d8e70dd41d6bac190c
enxajt <enxajt@gmail.com>
2017-03-22 16:04:04 +0100

    try sed in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -39,8 +39,9 @@ gulp.task('ejs', function() {
       var css = title+'.css';
       var sed = fs.readFileSync("./src/impress/replace.sh", "utf8");
       gulp.src(["./ejs/index.html","!./ejs/*.ejs"])
-        .pipe(exec('echo test > ./ttss'))
         .pipe(console.log('test: '+title))
+        //.pipe(exec('cat '+file.path+' > '+sed+' > ./src/impress/'+title+'_pages.ejs'))
+        //.pipe(exec("ckt '"+file.path+"' > ./src/impress/"+title+"_pages.ejs"))
         .pipe(console.log('test: '+title))
         .pipe(exec('echo test'))
         .pipe(exec('echo test > ./ttss'))
